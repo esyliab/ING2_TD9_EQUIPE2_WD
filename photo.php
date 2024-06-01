7bc084261e0484918ab92d0b37cb40c44282bfa6
Manon Loirat <manon.loirat@edu.ece.fr>
2024-06-01 21:26:01 +0200

    Manon - évènement

---
+++ photo.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.html");
+    exit();
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ECE_Social_Media";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$user_id = $_SESSION['user_id'];
+$title = $_POST['title'];
+$lieu = $_POST['lieu'];
+$image = $_POST['image'];
+$post_date = $_POST['date']; 
+
+$sql = "INSERT INTO Posts (user_id, title, lieu, image, created_at) VALUES (?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("issss", $user_id, $title, $lieu, $image, $post_date);
+
+if ($stmt->execute()) {
+    header("Location: accueil.html"); 
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
